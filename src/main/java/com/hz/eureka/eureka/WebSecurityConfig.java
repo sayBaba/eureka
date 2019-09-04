0e3d6a081a83f36d2ea698eae904f52297944061
sayBaba <answer123@foxmail.com>
2019-09-04 18:45:51 +0800

    eureka

---
+++ src/main/java/com/hz/eureka/eureka/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.hz.eureka.eureka;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * 在eurka服务端添加一个安全认证类
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        http.csrf().disable();
+        //注意：为了可以使用 http://${user}:${password}@${host}:${port}/eureka/ 这种方式登录,所以必须是httpBasic,
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
